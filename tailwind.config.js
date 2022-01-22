64b61ae530eaaeb80fbda53ac993648c4bedebeb
Gabriel Dias <86886134+DiasBriel@users.noreply.github.com>
2022-01-21 21:03:54 -0300

    Revert "Start the login page style"

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontSize: {
-        h1: "2.625rem",
-      },
-      fontFamily: {
-        outfit: ["Outfit"],
-      },
-      colors: {
-        primary: "#54B080",
-        subtitle: "#C8C8C8",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
